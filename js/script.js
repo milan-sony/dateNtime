f48180d317c348b6333830360bebc102a53f25b0
Milan Sony <milansonyofficial@gmail.com>
2023-11-25 14:32:58 +0530

    modified, bug fixed

--- js/script.js
+++ js/script.js
@@ -1,27 +1,67 @@
-// current date
-let currentDate = new Date();
-let year = currentDate.getFullYear();
-let month = currentDate.getMonth() + 1; 
-//months are zero-based, so adding 1
-let day = currentDate.getDate();
+//setting date and time
+setInterval(() =>{
+    // current date
+    let currentDate = new Date();
+    let year = currentDate.getFullYear();
+    let month = currentDate.getMonth() + 1; 
+    //months are zero-based, so adding 1
+    let day = currentDate.getDate();
 
-let formattedDate = `${day}/${month}/${year}`;
+    let formattedDate = `${day}/${month}/${year}`;
 
-let todayDate = document.getElementById("date");
-todayDate.innerHTML = formattedDate;
+    let todayDate = document.getElementById("date");
+    todayDate.innerHTML = formattedDate;
 
-// today's day
-let toDaySpan = document.getElementById("today")
-let todayObj = new Date()
-let dayNumber = todayObj.getDay()
-let daylist = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-let today = daylist[dayNumber]
-toDaySpan.innerHTML = today
+    // today's day
+    let toDaySpan = document.getElementById("today")
+    let todayObj = new Date()
+    let dayNumber = todayObj.getDay()
+    let daylist = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    let today = daylist[dayNumber]
+    toDaySpan.innerHTML = today
+}, 1000)
+
+//day wish
+setInterval(() =>{
+    var currentTime = new Date();
+    var currentHour = currentTime.getHours();
+    var greeting;
+
+    if (currentHour >= 5 && currentHour < 12) {
+        greeting = 'Good Morning';
+    } else if (currentHour >= 12 && currentHour < 17) {
+        greeting = 'Good Afternoon';
+    } else {
+        greeting = 'Good Evening';
+    }
+    
+    document.getElementById('greeting').innerText = greeting;
+}, 1000)
+
+// current time
+setInterval(() =>{
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var period = hours >= 12 ? 'PM' : 'AM';
+
+    //convert to 12-hour format
+    hours = hours % 12;
+    hours = hours ? hours : 12; //Handle midnight (12 AM)
+
+    //add leading zero to minutes and seconds if needed
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    var timeString = hours + ':' + minutes + ':' + seconds + ' ' + period;
+
+    document.getElementById('time').innerHTML = timeString;
+}, 1000)
 
 //setting the background image
-let bgImg = document.getElementById("bg-img");
-
 function setBackgroundImage() {
+    let bgImg = document.getElementById("bg-img");
     //URL of the image
     let imageUrl = 'https://picsum.photos/1920/1080';
     //fetch the image using fetch()
@@ -48,50 +88,3 @@ function setBackgroundImage() {
 setBackgroundImage();
 // call the function to set the background image in every 2 min
 setInterval(setBackgroundImage, 120000);
-
-//day wish
-function getGreeting() {
-    var currentTime = new Date();
-    var currentHour = currentTime.getHours();
-
-    var greeting;
-
-    if (currentHour >= 5 && currentHour < 12) {
-        greeting = 'Good Morning';
-    } else if (currentHour >= 12 && currentHour < 17) {
-        greeting = 'Good Afternoon';
-    } else {
-        greeting = 'Good Evening';
-    }
-
-    return greeting;
-}
-
-document.getElementById('greeting').innerText = getGreeting();
-
-// current time
-function updateClock() {
-    var currentTime = new Date();
-    var hours = currentTime.getHours();
-    var minutes = currentTime.getMinutes();
-    var seconds = currentTime.getSeconds();
-    var period = hours >= 12 ? 'PM' : 'AM';
-
-    //convert to 12-hour format
-    hours = hours % 12;
-    hours = hours ? hours : 12; //Handle midnight (12 AM)
-
-    //add leading zero to minutes and seconds if needed
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-
-    var timeString = hours + ':' + minutes + ':' + seconds + ' ' + period;
-
-    document.getElementById('time').innerHTML = timeString;
-}
-
-//update the clock every second
-setInterval(updateClock, 1000);
-
-//initial update
-updateClock();
